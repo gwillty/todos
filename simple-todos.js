100cbc79bbe53f97164ead36187d79365dd7d13b
gwillty <guilgarciajr@hotmail.com>
2015-05-02 15:56:07 +0800

    added Session

--- simple-todos.js
+++ simple-todos.js
@@ -5,8 +5,22 @@ if (Meteor.isClient) {
 	//this code runs on the client
 	Template.body.helpers({
 		tasks: function () {
-			return Tasks.find({},{sort: {createdAt: -1}});
+			if (Session.get("hideCompleted")) {
+				return Tasks.find({checked:{$ne: true}}, {sort:{createdAt: -1}});
+			} else {
+				return Tasks.find({},{sort: {createdAt: -1}});
+			}
+		
+		},
+		hideCompleted: function () {
+				return Session.get("hideCompleted");
+				
+		},
+		incompleteCount: function () {
+			return Tasks.find({checked: {$ne: true}}).count();
+			
 		}
+		
 	});
 	
 	Template.body.events({
@@ -25,8 +39,10 @@ if (Meteor.isClient) {
 			event.target.text.value = "";
 			//prevent default form submit;
 			return false;
+		},
+		"change .hide-completed input" : function (event) {
+				Session.set("hideCompleted", event.target.checked);
 		}
-		
 		
 	});
 	
